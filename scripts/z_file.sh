4e61a2714e3f7e5491632cad41cf33514c194eff
donhk <hk.fred.0@gmail.com>
2020-01-23 08:41:00 -0600

    improvements to install script

--- scripts/z_file.sh
+++ scripts/z_file.sh
@@ -3,7 +3,8 @@
 #
 # ffmpeg 4.2.2
 # https://johnvansickle.com/ffmpeg/
-echo "Installing some tools"
+echo "Installing some tools";
+
 export STREAM_TOOLS=${ISTREAM_HOME}/tools
 
 rm -rf ${STREAM_TOOLS}/ffmpeg
@@ -16,22 +17,36 @@ chown -R ${LOCAL_USERNAME}:${LOCAL_USERNAME} ${STREAM_TOOLS}
 #
 # ssh passwordless connection
 #
-export SSH_PUB=${HOME}/.ssh/id_rsa
-if [[ -f "${SSH_PUB}" ]]; then
+
+export LOCAL_SERVER_IDENTITY=${HOME}/.ssh/id_rsa
+
+if [[ -f "${LOCAL_SERVER_IDENTITY}" ]]; then
     echo "id_rsa exists";
 else
     echo "creating id_rsa";
-    ssh-keygen -f ${HOME}/.ssh/id_rsa -P '' -N '' -q <<< y
+    ssh-keygen -f ${LOCAL_SERVER_IDENTITY} -P '' -N '' -q <<< y
 fi
 
-ssh-keyscan -t rsa -H ${REMOTE_SERVER_ADDRES} >> ${HOME}/.ssh/known_hosts
-sshpass -p ${REMOTE_SERVER_PASS} | ssh-copy-id -i ${HOME}/.ssh/id_rsa.pub ${REMOTE_SERVER_USER}@${REMOTE_SERVER_ADDRES}
+if [["" == $(ssh-keygen -H -F ${REMOTE_SERVER_ADDRES})]]; then
+    echo 'Adding remote machine to known_hosts';
+    ssh-keyscan -t rsa -H ${REMOTE_SERVER_ADDRES} >> ${HOME}/.ssh/known_hosts
+else
+    echo 'remote key already present on known_hosts';
+fi
 
-export LOCAL_SERVER_IDENTITY=${HOME}/.ssh/id_rsa
+if [[ "yes" == "${CONFIGURE_PASSWORDLESS_SSH}" ]]; then
+    echo 'Copying pub key to server';
+    sshpass -p ${REMOTE_SERVER_PASS} | ssh-copy-id -i ${HOME}/.ssh/id_rsa.pub ${REMOTE_SERVER_USER}@${REMOTE_SERVER_ADDRES}
+else
+     echo 'Assuming passwordless ssh woth server'
+fi
+
 #
 # sshfs
 #
+
 export FUSE_FILE=/etc/fuse.conf
+
 if grep -q "^user_allow_other" "${FUSE_FILE}"; then
     echo "All set with ${FUSE_FILE}";
 else
@@ -39,18 +54,16 @@ else
     echo "user_allow_other" >> /etc/fuse.conf
 fi
 
-su - ${LOCAL_USERNAME} <<EOF
-ssh-keygen -R ${REMOTE_SERVER_ADDRES}
-ssh-keyscan -H ${REMOTE_SERVER_ADDRES} >> ~/.ssh/known_hosts
-EOF
-
 # make sure it is not being used
 fusermount -u ${LOCAL_SERVER_DIRECTORY}
 
+export FSTAB=/etc/fstab
 export MUID=$(id -u ${LOCAL_USERNAME})
 export MGID=$(id -g ${LOCAL_USERNAME})
-export OPTIONS="allow_other,default_permissions,reconnect,nonempty,uid=${MUID},gid=${MGID},IdentityFile=${LOCAL_SERVER_IDENTITY}"
-sshfs ${REMOTE_SERVER_USER}@${REMOTE_SERVER_ADDRES}:${REMOTE_SERVER_DIRECTORY} ${LOCAL_SERVER_DIRECTORY} -o ${OPTIONS}
+export OPTIONS="noauto,x-systemd.automount,_netdev,allow_other,default_permissions,reconnect,nonempty,uid=${MUID},gid=${MGID},IdentityFile=${LOCAL_SERVER_IDENTITY}"
+echo "sshfs#${REMOTE_SERVER_USER}@${REMOTE_SERVER_ADDRES}:${REMOTE_SERVER_DIRECTORY} ${LOCAL_SERVER_DIRECTORY} fuse ${OPTIONS} 0 0" >> ${FSTAB}
+
+mount -a
 
 #
 # deploy project
@@ -65,7 +78,9 @@ npm install -g .
 #
 # create unit
 #
+
 export UNIT_FILE=/etc/systemd/system/istream.service
+
 touch ${UNIT_FILE}
 truncate --size 0 ${UNIT_FILE}
 
@@ -92,17 +107,15 @@ systemctl daemon-reload
 #
 # creating app properties
 #
+
 export APP_PROPERTIES=bin/.app.properties
+
 cat <<EOF >>${APP_PROPERTIES}
 CAM_USER=${CAM_USER}
 CAM_PASS=${CAM_PASS}
-REMOTE_SERVER_ADDRES=${REMOTE_SERVER_ADDRES}
-REMOTE_SERVER_USER=${REMOTE_SERVER_USER}
-REMOTE_SERVER_PASS=${REMOTE_SERVER_PASS}
 REMOTE_SERVER_DIRECTORY=${REMOTE_SERVER_DIRECTORY}
 AVAILABLE_STORAGE=${AVAILABLE_STORAGE}
 LOCAL_SERVER_DIRECTORY=${LOCAL_SERVER_DIRECTORY}
-LOCAL_SERVER_IDENTITY=${LOCAL_SERVER_IDENTITY}
 LOCAL_USERNAME=${LOCAL_USERNAME}
 CLEAN_DIR_INTERVAL=${CLEAN_DIR_INTERVAL}
 MAX_USED_SPACE=${MAX_USED_SPACE}
